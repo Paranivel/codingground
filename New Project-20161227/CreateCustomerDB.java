8720d6d4c40c6f5514c2ba915bd576bbbf685880
Paranivel <ponparanivel@gmail.com>
2016-12-28 12:20:52 +0530

    Created by Coding Ground Client

---
+++ New Project-20161227/CreateCustomerDB.java
@@ -0,0 +1,24 @@
+import java.sql.*;
+public class CreateCustomerDB
+{
+   static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+   static final String DB_URL = "jdbc:mysql://localhost/CODINGGROUND";
+   static final String USER = "root";
+   static final String PASS = "root";
+   
+   public static void main(String[] args) {
+      Connection conn = null;
+      Statement st = null;
+
+   try{
+      Class.forName("com.mysql.jdbc.Driver");
+      System.out.println("Connecting to database...");
+      conn = DriverManager.getConnection(DB_URL,USER,PASS);
+      st = conn.createStatement();
+      st.executeQuery("Create table customertable(customeridtab int,customernametab varchar(50),dobtab date,balancetab decimal(10,2))");
+   }
+   catch(Exception e){
+       System.out.println("Error While creating CustomerDB" + e);
+   }
+ }
+}
